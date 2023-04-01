9500bb99c8c71c370b30d14dccf3996fb1684455
Queue2109 <hj2109@student.uni-lj.si>
2023-04-01 13:22:03 +0200

    Base structure of our app

---
+++ HealthApp/app/src/main/java/com/example/healthapp/FirstPageFragment.kt
@@ -0,0 +1,19 @@
+package com.example.healthapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class FirstPageFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first_page, container, false)
+    }
+}
---
+++ HealthApp/app/src/main/java/com/example/healthapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.healthapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_first_page)
+    }
+}
