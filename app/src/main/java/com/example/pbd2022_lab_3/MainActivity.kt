8b1d0d59a27de440135e51296b80923c4d12540d
zala <zalapregeljkp@gmail.com>
2023-04-01 14:03:39 +0200

    osnova

--- app/src/main/java/com/example/pbd2022_lab_3/MainActivity.kt
+++ app/src/main/java/com/example/pbd2022_lab_3/MainActivity.kt
@@ -1,33 +1,11 @@
 package com.example.pbd2022_lab_3
 
-import android.os.Bundle
-import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.tabs.TabLayoutMediator
-import com.example.pbd2022_lab_3.TabPagerAdapter
-
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
-    companion object {
-        private const val TAG = "MainActivity"
-        private const val NUM_OF_TABS = 3
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        setSupportActionBar(toolbar)
-
-        configureTabLayout()
     }
-
-    private fun configureTabLayout() {
-
-        // TODO: Implement tab-viewpager-adapter connection
-
-    }
-
-
 }
